179135dc1385df405362da2c6a26adf5c5a7da67
Mitchel Koster <mitchelkoster@gmail.com>
2024-12-12 01:18:11 +0100

    sort numbers

--- day_1/challenge_1_test.go
+++ day_1/challenge_1_test.go
@@ -4,13 +4,15 @@ import (
 	"adventofcode-2024/utils"
 	"fmt"
 	"regexp"
+	"sort"
 	"strconv"
 	"testing"
 )
 
 type ListItem struct {
-	Left  int
-	Right int
+	Left     int
+	Right    int
+	Distance int
 }
 
 func ChallengeParser(content []string) ([]ListItem, error) {
@@ -34,10 +36,41 @@ func ChallengeParser(content []string) ([]ListItem, error) {
 			return nil, err
 		}
 
-		numberListItem = append(numberListItem, ListItem{Left: Left, Right: second})
+		numberListItem = append(numberListItem, ListItem{
+			Left:     Left,
+			Right:    second,
+			Distance: 0,
+		})
 	}
 
 	return numberListItem, nil
+}
+
+func OrderListemsAscending(listItems []ListItem) ([]ListItem, error) {
+	leftList, rightList := []int{}, []int{}
+	for _, item := range listItems {
+		leftList = append(leftList, item.Left)
+		rightList = append(rightList, item.Right)
+	}
+
+	if !sort.IntsAreSorted(leftList) {
+		sort.Ints(leftList)
+	}
+
+	if !sort.IntsAreSorted(rightList) {
+		sort.Ints(rightList)
+	}
+
+	newListItems := []ListItem{}
+	for i, _ := range leftList {
+		newListItems = append(newListItems, ListItem{
+			Left:     leftList[i],
+			Right:    rightList[i],
+			Distance: 0, // TODO: Let's be lazy and take in distance
+		})
+	}
+
+	return newListItems, nil
 }
 
 func TestParseChallengeFile(t *testing.T) {
@@ -57,7 +90,37 @@ func TestParseChallengeFile(t *testing.T) {
 		t.Fatalf("Expected 6 ListItems, got %d", len(listItems))
 	}
 
-	if Left, Right := listItems[0].Left, listItems[0].Right; Left != 3 || Right != 4 {
-		t.Fatalf("Left ListItem is incorrect: expected (3, 4), got (%d, %d)", Left, Right)
+	if left, right := listItems[0].Left, listItems[0].Right; left != 3 || right != 4 {
+		t.Fatalf("Left ListItem is incorrect: expected (3, 4), got (%d, %d)", left, right)
 	}
+}
+
+func TestOrderAscendingByList(t *testing.T) {
+	fileUtils := utils.FileUtils{}
+	content, err := fileUtils.ReadFileLineByLine("example.txt", nil)
+
+	if err != nil {
+		t.Fatalf("fileUtils.ReadFileContents failed: %v", err)
+	}
+
+	listItems, err := ChallengeParser(content)
+	if err != nil {
+		t.Fatalf("ChallengeParser failed: %v", err)
+	}
+
+	for _, val := range listItems {
+		fmt.Println(val)
+	}
+	fmt.Println("====")
+
+	orderedListItems, err := OrderListemsAscending(listItems)
+	if err != nil {
+		t.Fatalf("OrderListemsAscending failed: %v", err)
+	}
+
+	// Test first row
+	if left, right := orderedListItems[0].Left, orderedListItems[0].Right; left != 1 && right != 3 {
+		t.Fatalf("Left ListItem is incorrect: expected (1, 3), got (%d, %d)", left, right)
+	}
+
 }
