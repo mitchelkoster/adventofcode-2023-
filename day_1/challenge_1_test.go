2807d577d8245db74cca854443e37594bf07bece
Mitchel Koster <mitchelkoster@gmail.com>
2024-12-17 00:24:01 +0100

    sorted package mess

--- day_1/challenge_1_test.go
+++ day_1/challenge_1_test.go
@@ -1,87 +1,10 @@
-package main_test
+package day_1_test
 
 import (
+	"adventofcode-2024/day_1"
 	"adventofcode-2024/utils"
-	"fmt"
-	"math"
-	"regexp"
-	"sort"
-	"strconv"
 	"testing"
 )
-
-type ListItem struct {
-	Left     int
-	Right    int
-	Distance int
-}
-
-func ChallengeParser(content []string) ([]ListItem, error) {
-	numberListItem := []ListItem{}
-	re := regexp.MustCompile(`\d+`)
-
-	for _, line := range content {
-		matches := re.FindAllString(string(line), -1)
-
-		if len(matches) < 2 {
-			return nil, fmt.Errorf("Only one match found on line")
-		}
-
-		first, err := strconv.Atoi(matches[0])
-		if err != nil {
-			return nil, err
-		}
-
-		last, err := strconv.Atoi(matches[1])
-		if err != nil {
-			return nil, err
-		}
-
-		numberListItem = append(numberListItem, ListItem{
-			Left:     first,
-			Right:    last,
-			Distance: 0,
-		})
-	}
-
-	return numberListItem, nil
-}
-
-func OrderListemsAscending(listItems []ListItem) ([]ListItem, error) {
-	leftList, rightList := []int{}, []int{}
-	for _, item := range listItems {
-		leftList = append(leftList, item.Left)
-		rightList = append(rightList, item.Right)
-	}
-
-	if !sort.IntsAreSorted(leftList) {
-		sort.Ints(leftList)
-	}
-
-	if !sort.IntsAreSorted(rightList) {
-		sort.Ints(rightList)
-	}
-
-	newListItems := []ListItem{}
-	for i, _ := range leftList {
-		newListItems = append(newListItems, ListItem{
-			Left:     leftList[i],
-			Right:    rightList[i],
-			Distance: int(math.Abs(float64(leftList[i]) - float64(rightList[i]))),
-		})
-	}
-
-	return newListItems, nil
-}
-
-func SumOfDistances(listItems []ListItem) int {
-	sum := 0
-	for _, item := range listItems {
-		sum += item.Distance
-	}
-
-	return sum
-}
 
 func TestParseChallengeFile(t *testing.T) {
 	fileUtils := utils.FileUtils{}
@@ -91,7 +14,7 @@ func TestParseChallengeFile(t *testing.T) {
 		t.Fatalf("fileUtils.ReadFileContents failed: %v", err)
 	}
 
-	listItems, err := ChallengeParser(content)
+	listItems, err := day_1.ChallengeParser(content)
 	if err != nil {
 		t.Fatalf("ChallengeParser failed: %v", err)
 	}
@@ -113,12 +36,12 @@ func TestChallengeSolution(t *testing.T) {
 		t.Fatalf("fileUtils.ReadFileContents failed: %v", err)
 	}
 
-	listItems, err := ChallengeParser(content)
+	listItems, err := day_1.ChallengeParser(content)
 	if err != nil {
 		t.Fatalf("ChallengeParser failed: %v", err)
 	}
 
-	orderedListItems, err := OrderListemsAscending(listItems)
+	orderedListItems, err := day_1.OrderListemsAscending(listItems)
 	if err != nil {
 		t.Fatalf("OrderListemsAscending failed: %v", err)
 	}
@@ -149,7 +72,7 @@ func TestChallengeSolution(t *testing.T) {
 	}
 
 	// Sum of weights (why is  there no map, reduce or sum?!?!)
-	if SumOfDistances(orderedListItems) != 11 {
-		t.Fatalf("Expeced (11), got (%d)", SumOfDistances(orderedListItems))
+	if day_1.SumOfDistances(orderedListItems) != 11 {
+		t.Fatalf("Expeced (11), got (%d)", day_1.SumOfDistances(orderedListItems))
 	}
 }
